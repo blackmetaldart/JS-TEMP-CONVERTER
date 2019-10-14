0147e7780bd0716bb11078189635e07741bb91c6
Alexander <mrbeachcode@gmail.com>
2019-10-14 19:25:10 -0400

    my js temp converter

---
+++ script.js
@@ -0,0 +1,56 @@
+
+document.getElementById('celsButton').addEventListener('click', celsTransfer );
+document.getElementById('kelvButton').addEventListener('click', kelvTransfer);
+document.getElementById('farenButton').addEventListener('click', farenTransfer);
+
+
+function celsTransfer () {
+  var celsius = parseInt(document.getElementById('userNum').value, 10);
+
+  let transBox = document.getElementById('transferTemp1');
+  let transBox2 = document.getElementById('transferTemp2')
+
+  document.getElementById('taip1').innerText = "FAHRENHEIT";
+  document.getElementById('taip2').innerText = "KELVIN";
+
+  const newKelv = (celsius + 273.15);
+  var newFaren = (celsius * 1.8) + 32;
+
+  document.getElementById('one').innerText = `${newFaren}`;
+  document.getElementById('two').innerText  = `${newKelv}`;
+}
+
+function kelvTransfer () {
+  var kelvin = parseInt(document.getElementById('userNum').value, 10);
+
+  let transBox = document.getElementById('transferTemp1');
+  let transBox2 = document.getElementById('transferTemp2');
+
+  document.getElementById('taip1').innerText = "CELSIUS";
+  document.getElementById('taip2').innerText = "FAHRENHEIT";
+
+  const newCels = (kelvin - 273.15) ;
+  var newFaren = ((kelvin - 273) * 1.8) + 32 ;
+
+  document.getElementById('one').innerText = `${newCels}`;
+  document.getElementById('two').innerText  = `${newFaren}`;
+}
+
+
+
+function farenTransfer () {
+  var farenheit = parseInt(document.getElementById('userNum').value, 10);
+
+  let transBox = document.getElementById('transferTemp1');
+  let transBox2 = document.getElementById('transferTemp2');
+
+  document.getElementById('taip1').innerText = "KELVIN";
+  document.getElementById('taip2').innerText = "CELSIUS";
+
+
+  var newKelv = ((farenheit - 32) /1.8) + 273.15;
+  const newCels = (farenheit - 32) / (9/5);
+
+  document.getElementById('one').innerText = `${newKelv}`;
+  document.getElementById('two').innerText  = `${newCels}`;
+}
